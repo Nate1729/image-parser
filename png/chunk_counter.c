37b987d49948aba105c780cc9ce7af2c49d36904
Nate Ferreira <nferreira1729@gmail.com>
2024-01-20 17:53:32 -0500

    count chunks and write them to stdout

--- png/chunk_counter.c
+++ png/chunk_counter.c
@@ -1,14 +1,36 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
 
 #include "png_parser.h"
 
 const char *FILE_NAME = "image.png";
 
 typedef struct {
-  unsigned length;
-} PNG_Chunk;
+  unsigned int length;
+  char type[4];
+} ChunkHeader;
+
+/* Read the length and type of the current chunk.
+ * Edits the `ChunkHeader` in-place.
+ * Returns 0 for success and 1 for failure.
+ * */
+int read_chunk_header(FILE *f, ChunkHeader *header) {
+  unsigned int raw_length;
+  if (fread(&raw_length, sizeof(unsigned int), 1, f) < 1) {
+    fprintf(stderr, "Error reading chunk header.\n");
+    return 1;
+  }
+  header->length = ntohl(raw_length);
+
+  if (fread(header->type, sizeof(char), 4, f) < 4) {
+    fprintf(stderr, "Erro reading chunk header.\n");
+    return 1;
+  }
+
+  return 0;
+}
 
 int main() {
   FILE *f = fopen(FILE_NAME, "rb");
@@ -23,35 +45,37 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  /* Read the first chunk */
-  char chunk_type[5] = {0, 0, 0, 0, 0};
-  unsigned int chunk_length = 0;
-  while (1) {
-    /* Skipping over data and CRC value*/
-    if (fseek(f, chunk_length + 4, SEEK_CUR)) {
+  ChunkHeader chunk_header;
+  if (read_chunk_header(f, &chunk_header)) {
+    fprintf(stderr, "Had problem reading chunk error -- Exiting early.\n");
+    fclose(f);
+    return EXIT_FAILURE;
+  }
+
+  unsigned chunk_count = 1;
+  while (convert_char_to_chunk_type(chunk_header.type) != CHUNK_TYPE_IEND) {
+    /* Debug stuff
+    fprintf(stdout, "Chunk type: %.4s\n", chunk_header.type);
+    fprintf(stdout, "Chunk length: %i\n", chunk_header.length);
+    */
+
+    if (fseek(f, chunk_header.length + 4, SEEK_CUR)) {
       fprintf(stderr, "Error skipping over data.\n");
       fclose(f);
       return EXIT_FAILURE;
     }
 
-    /* Unfortunately, network byte order screws us */
-    if (fread(&chunk_length, sizeof(unsigned int), 1, f) < 1) {
-      fprintf(stderr, "Error reading chunk length.\n");
-      fclose(f);
-      return EXIT_FAILURE;
-    }
-    chunk_length = ntohl(chunk_length);
-
-    if (fread(chunk_type, sizeof(char), 4, f) < 4) {
-      fprintf(stderr, "Error reading chunk type.\n");
+    if (read_chunk_header(f, &chunk_header)) {
+      fprintf(stderr, "Had problem reading chunk error -- Exiting early.\n");
       fclose(f);
       return EXIT_FAILURE;
     }
 
-    printf("Chunk type: %s\n", chunk_type);
-    printf("Chunk length: %i\n", chunk_length);
+    chunk_count += 1;
   }
 
   fclose(f);
+
+  printf("Total Chunks: %i\n", chunk_count);
   return 0;
 }
